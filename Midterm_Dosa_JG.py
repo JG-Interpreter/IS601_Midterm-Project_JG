e5a008b288007c7f5fc2d408e5625ccf627dada0
Glenn Jordan <you@example.com>
2025-02-23 14:48:44 -0500

    add final updates to code and commenting

--- Midterm_Dosa_JG.py
+++ Midterm_Dosa_JG.py
@@ -2,7 +2,9 @@ import json
 
 class DosaOrderParse():
     """
-        Explain context of code
+        order_parse function: entire function reads in json file (filename as arguement) from Dosa restuarant and performs two operations. 
+        The first operation reads customer info and outputs another json file with phone # and customer name. 
+        Second operation reads items ordered and outputs a json file with oder item name, price of item, and count of orders of such type. 
     """
 
     def order_parse(filename):
@@ -10,6 +12,7 @@ class DosaOrderParse():
             with open(filename, 'r') as f:
                 data = json.load(f)
 
+            #check if data in list (json array format)
             if not isinstance(data, list) or not data:
                 print("Error: Input JSON file does not contain a valid array.")
                 return
@@ -18,7 +21,9 @@ class DosaOrderParse():
         except json.JSONDecodeError: print(f"Error: Invalid JSON format in '{filename}'.")
         
         """
-            Explain code block below
+            Customers.json output: establish customer info as an empty dictionary. When reading the JSON file, 
+            identfy instances of a dictionary with name and phone as keys. Combine the two objects with phone as key and
+            name as value for output to new json using the 'w' write mode. 
         """
 
         customer_info = {}
@@ -32,14 +37,17 @@ class DosaOrderParse():
             json.dump(customer_info, f)
 
         """
-            Explain code block below
+            items.json output: create another empty dictionary. Since items are an array nested within json a dictionary, 
+            iterate trhough each order and identify instances of item lists inside of each dictionary format. 
+            Identify keys: name and store as new dictionary key. 
+            Next, price and count of orders will nested as a dictionay within each order dictionary occurence.
+            lastly, output will be written to a new json using using the 'w' write mode. 
         """
         orders = {}
         for order_data in data:
             if isinstance(order_data, dict) and 'items' in order_data and isinstance(order_data['items'], list):
                 for item in order_data['items']:
                     if isinstance(item, dict) and 'name' in item and 'price' in item:
-                        # orders[item['name']] = item['price']
                         item_name = item['name']
                         item_price = item['price']
                         if item_name in orders:
